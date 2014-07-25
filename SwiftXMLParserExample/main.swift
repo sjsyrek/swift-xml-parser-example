c5739ba25dc766c826550d50dd58683279279caf
Steven Syrek <steven.syrek@gmail.com>
2014-07-24 22:51:23 -0400

    Initial Commit

---
+++ SwiftXMLParserExample/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftXMLParserExample
+//
+//  Created by Steven Syrek on 7/24/14.
+//  Copyright (c) 2014 Steven Syrek. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
